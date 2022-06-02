b0ab9b14a7fcd7af6d3b2982280f845e2e087bc7
Gab-Serrano <gab.serrano@duocuc.cl>
2022-06-02 11:18:16 -0400

    color mati

--- Estudiantes.py
+++ Estudiantes.py
@@ -4,3 +4,5 @@ if color == "azul":
     print("Este es el color de Carlos.")
 elif color == "amarillo":
     print("Este es el color de Renato.")
+elif color == "morado":
+    print("Este es el color de Matías.")
